1d9f1fc6a22cef0bffb87184d89c5e196a7f7e84
Kristina Song <kristina.song@usherbrooke.ca>
2022-05-17 10:27:33 -0400

    redirect slurm output files

--- scripts/slurmSubmit.py
+++ scripts/slurmSubmit.py
@@ -29,6 +29,13 @@ cmdline += jobscript
 # remove the leading and trailing white space for the submitted jobid
 cmdline += r" | awk '{print substr($NF, 0, length($NF))}'"
 
+# direct slurm output and error file to a specific directory
+slurm_outdir = '../network_output/slurmout'
+if not os.path.exists(slurm_outdir):
+    os.makedirs(slurm_outdir)
+    
+cmdline += " --output=../network_output/slurmout/%j.out --error=../network_output/slurmout/%j.err"
+
 sys.stdout.write(cmdline)
 
 os.system(cmdline)
