dac9e8eb13341702f6c62ad07f26ad65e89d512d
Kristina Song <kristina.song@usherbrooke.ca>
2022-05-25 22:23:13 -0400

    rank by num of copies and TPM done

--- scripts/rank_sno.py
+++ scripts/rank_sno.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+from cmath import nan
 import pandas as pd
+from scipy.fftpack import idctn
+pd.options.mode.chained_assignment = None  # default='warn'
 import numpy as np
 import sys
 import math
@@ -11,19 +14,38 @@ def blastn(copy_list):
     # Compare % identity between snoRNA copies
     return
 
-def get_copies(id,curr_rfam,snodb):
-    # Get all snoRNA copies by RfamID
-    df_copies = snodb.loc[snodb['rfam_id'] == curr_rfam]
-    copies = df_copies[['ensembl_id','refseq_id','gene_name']].values.tolist()
-    return copies
-
-def get_sno_info(id,name,tpm,snodb):
-    # Compute TPM values (AVG, MIN, MAX) for specified snoRNA.
+def get_tpm(id,tpm):
+    # Compute TPM metrics for snoRNA
     tpm_genes = tpm.iloc[:,0]
     values = tpm.loc[tpm_genes.isin([id]),].drop(['gene_id','gene_name'],axis=1).reset_index(drop=True).to_numpy()
     avg = np.mean(values)
     min = np.amin(values)
     max = np.amax(values)
+    return avg, min, max
+
+def get_copies(curr_rfam,snodb):
+    # Get all snoRNA copies by RfamID
+    df_copies = snodb.loc[snodb['rfam_id'] == curr_rfam]
+    copies = df_copies[['ensembl_id','refseq_id','gene_name']].values.tolist()
+    return copies
+
+def verify_copies(copies,tpm):
+    # Check if copies are highly expressed (avg TPM >= 10)
+    exp_copies = []
+    for c in copies:
+        id = c[0]
+        if isinstance(id, float): # check for nan in ensembl_id column
+            id = c[1]
+        if isinstance(id, float): # check for nan in ensembl_id column
+            avg,_,_ = get_tpm(c[2],tpm,'name') # get TPM by name
+        #avg,_,_ = get_tpm(id,tpm)
+        #if avg >= 10:
+            #exp_copies.append(id)
+    return 0 #len(exp_copies)
+
+def get_sno_info(id,name,tpm,snodb):
+    # Compute TPM values (AVG, MIN, MAX) for specified snoRNA.
+    avg, min, max = get_tpm(id,tpm)
     # round numbers 
     rounded = []
     for num in [avg,min,max]:
@@ -34,6 +56,7 @@ def get_sno_info(id,name,tpm,snodb):
     avg = rounded[0]
     min = rounded[1]
     max = rounded[2]
+
     # Get other snoRNA characteristics including number of copies based on RfamID
     snodb_id = snodb.loc[snodb['ensembl_id'].str.contains(id,na=False)].reset_index(drop=True)
     rfam_id = snodb.loc[snodb['ensembl_id'].str.contains(id,na=False)].reset_index(drop=True)
@@ -43,26 +66,42 @@ def get_sno_info(id,name,tpm,snodb):
     else: # snoRNAs with id starting with "NR_"
         snodb_id = snodb.loc[snodb['refseq_id'].str.contains(id,na=False)].reset_index(drop=True).at[0,'snodb_id']
         rfam_id = snodb.loc[snodb['refseq_id'].str.contains(id,na=False)].reset_index(drop=True).at[0,'rfam_id']
-    copies_list = get_copies(id,rfam_id,snodb)
+    copies_list = get_copies(rfam_id,snodb)
     num_copies = len(copies_list)
+    # 0 --> no Rfam ID present for that snoRNA
     if num_copies != 0:
-        num_copies = num_copies-1
+        # remove self
+        num_copies = num_copies-1 
     result = pd.DataFrame({'snoRNA': name, 'Ensembl_ID/RefSeq_ID':id,'snoDB_ID': snodb_id, 'num_copies':[num_copies],'mean_TPM':[avg],'max_TPM':[max],'min_TPM':[min]})
     return result
 
 def rank_snornas(df):
-    # Rank snoRNAs by TPM
-    #df['TPM_rank'] = df[['mean_TPM','max_TPM','min_TPM']].apply(tuple,axis=1).rank(method='dense',ascending=False).astype(int)
-    #df = df.sort_values('TPM_rank')
-    #df = df.groupby('num_copies')
+    # remove snoRNAs with min TPM == 0 and max TPM < 10
+    df.drop(df[df.min_TPM < 1].index, inplace=True)
+    df.drop(df[df.max_TPM < 10].index, inplace=True)
+
     # group by number of copies --> rank within group by TPM
-    df["group_rank"] = df.groupby("num_copies")["mean_TPM"].rank(ascending=False)
-    df = df.sort_values(by=["num_copies","group_rank","min_TPM"])
-    # extract snoRNAs with max TPM < 50 (rank bottom)
-    tpm_lt = df.loc[df['max_TPM'] < 50]
-    tpm_ge = df.loc[~(df['max_TPM'] < 50)]
-    df = pd.concat([tpm_ge,tpm_lt],ignore_index=True)
-    return df
+    # get number of copies as list (for grouping)
+    num_cp_list = list(df.groupby("num_copies").groups.keys())
+    filtered_cp = []
+    for n in num_cp_list:
+        if n <= 10: # don't want snoRNAs with more than 10 copies
+            filtered_cp.append(n)
+
+    # rank by TPM values for each num_of_copy group
+    dic_df = {}
+    for num in filtered_cp:
+        df_temp = df.loc[df['num_copies'] == num]
+        df_temp['TPM_rank'] = df_temp[['mean_TPM','max_TPM','min_TPM']].apply(tuple,axis=1).rank(method='dense',ascending=False).astype(int)
+        df_temp = df_temp.sort_values('TPM_rank').reset_index(drop=True)
+        dic_df[str(num)] = df_temp
+    
+    # concat groups into one df
+    df_result = pd.DataFrame(columns=dic_df['0'].columns)
+    for num in filtered_cp:
+        df_result = pd.concat([df_result,dic_df[str(num)]],ignore_index=True)
+
+    return df_result
 
 def main():
     sno_file = sys.argv[1] # snoRNA list (min 2 columns: 'id', 'name', ...)
