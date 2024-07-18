7495285b5ce4f5d269a8839a2918632170fa9196
Kristina Song <kristina.song@usherbrooke.ca>
2024-07-18 16:45:56 -0400

    snoFlake pipeline complete

--- workflow/rules/cytoscape.smk
+++ workflow/rules/cytoscape.smk
@@ -1,4 +1,4 @@
-rule build_network:
+rule network_data:
     input:
         RBP_binds_to_sno = rules.RBP_binds_to_sno.output,
         STRING = rules.extract_STRING.output,
@@ -8,9 +8,30 @@ rule build_network:
     output:
         nodes = "results/networks/nodes.tsv",
         edges = "results/networks/edges.tsv"
+    params:
+        type = "data"
+    conda:
+        "../envs/python.yaml"
+    message:
+        "Gather node and edge data to construct the snoRNA-RBP interaction network via Cytoscape."
+    shell:
+        "pip install py4cytoscape && "
+        "python3 workflow/scripts/cytoscape.py {params.type} {output.nodes} {output.edges} {input.RBP_binds_to_sno} {input.STRING} "
+        "{input.sno_RBP_overlap} {input.snoRNA} {input.RBP}"
+
+
+rule build_network:
+    # Need Cytoscape app to be RUNNING
+    input:
+        nodes = rules.network_data.output.nodes,
+        edges = rules.network_data.output.edges
+    output:
+        network = "results/networks/snoFlake.cys"
+    params:
+        type = "build"
     conda:
         "../envs/python.yaml"
     message:
         "Build the snoRNA-RBP interaction network via Cytoscape."
-    script:
-        "../scripts/cytoscape.py"
+    shell:
+        "python3 workflow/scripts/cytoscape.py {params.type} {input.nodes} {input.edges} {output.network}"
--- workflow/scripts/cytoscape.py
+++ workflow/scripts/cytoscape.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import py4cytoscape as p4c
 import sys
+import numpy as np
 
 
 def node_weight(df):
@@ -29,30 +31,91 @@ def edge_weight(df):
     df.loc[(df['STRING_score'] >= 925) & (df['STRING_score'] <950),'weight'] = 10
     df.loc[(df['STRING_score'] >= 950) & (df['STRING_score'] <975),'weight'] = 100
     df.loc[df['STRING_score'] >= 975,'weight'] = 1000
+
+    # replace inf with 400 (sno_RBP_overlap)
+    df.replace(np.inf, 400, inplace=True)
+
     return df
 
 
+def build_network(nodes,edges,smk_network):
+    """
+    Build snoFlake using Cytoscape --> Cytoscape app has to be OPEN
+    """
+    p4c.create_network_from_data_frames(nodes, edges, title="all_interactions", collection="snoFlake")
+
+    # Create style
+    style_name = "snoFlake Style"
+    defaults = {'NODE_SHAPE':"ELLIPSE", 'NODE_TRANSPARENCY':255, 'NODE_BORDER_COLOR':"#252525", 'NODE_BORDER_WIDTH':3}
+    nodeLabels = p4c.map_visual_property('node label', 'name', 'p')
+    p4c.create_visual_style(style_name, defaults, [nodeLabels])
+    p4c.set_visual_style(style_name)
+    p4c.set_background_color_default('white', style_name='snoFlake Style')
+
+    # Node design
+    p4c.set_node_color_mapping('group', ['snoRNA', 'protein_coding'], ['#0C2C84', '#FECD87'], 'd', style_name='snoFlake Style')
+    p4c.set_node_size_mapping('score', ['1','10','100','1000'], [40,60,80,100], 'd', style_name='snoFlake Style')
+    p4c.set_node_label_color_mapping('group', ['snoRNA', 'protein_coding'], ['#0CE7D9', '#252525'], 'd', style_name='snoFlake Style')
+    p4c.set_node_font_size_mapping('group', ['snoRNA', 'protein_coding'], [15, 12], 'd', style_name='snoFlake Style')
+
+    # Edge design
+    p4c.set_edge_color_mapping('interaction', ['RBP_binds_to_snoRNA', 'STRING', 'sno_RBP_overlap'], ['#01665E', '#FEC55F', '#9F9F9F'], 'd', style_name='snoFlake Style')
+    p4c.set_edge_opacity_mapping('weight', ['1','10','100','1000'], [140,160,180,200], 'd', style_name='snoFlake Style')
+    p4c.set_edge_line_width_mapping('weight', ['1','10','100','1000'], [2,4,6,8], 'd', style_name='snoFlake Style')
+
+    # Network layout
+    p4c.layout_network('degree-circle')
+
+    p4c.save_session(smk_network) # save session
+
+    return
+
+
 def main():
+    
+    # Snakemake variables
+    smk_func = sys.argv[1]
+    smk_nodes = sys.argv[2]
+    smk_edges = sys.argv[3]
 
-    # NODES
-    sno = pd.read_csv(snakemake.input.snoRNA,sep='\t',usecols=['gene_id','gene_name','gene_biotype','min_TPM','max_TPM'])
-    rbp = pd.read_csv(snakemake.input.RBP,sep='\t',usecols=['gene_name','gene_biotype','min_TPM','max_TPM'])
-    rbp['gene_id'] = rbp['gene_name']
-    rbp = rbp[['gene_id','gene_name','gene_biotype','min_TPM','max_TPM']]
 
-    nodes = pd.concat([sno,rbp],ignore_index=True)
-    nodes.gene_name.fillna(nodes.gene_id, inplace=True)
-    nodes = node_weight(nodes)
-    nodes.to_csv(snakemake.output.nodes,sep='\t',index=None)
+    if  smk_func == "data": # Get node and edge data
 
-    # EDGES
-    rbp_binds_to_sno = pd.read_csv(snakemake.input.RBP_binds_to_sno[0],sep='\t')
-    sno_rbp_ovlp = pd.read_csv(snakemake.input.sno_RBP_overlap,sep='\t')
-    string = pd.read_csv(snakemake.input.STRING[0],sep='\t')
-    edges = pd.concat([rbp_binds_to_sno,sno_rbp_ovlp,string],axis=0,ignore_index=True)
-    edges = edges[['source','target','interaction','ENCODE_log_pval','sno_RBP_overlap_log_pval','STRING_score']]
-    edges = edge_weight(edges)
-    edges.to_csv(snakemake.output.edges,sep='\t',index=None)
+        smk_RBP_binds_to_sno = sys.argv[4]
+        smk_STRING = sys.argv[5]
+        smk_sno_RBP_overlap = sys.argv[6]
+        smk_snoRNA = sys.argv[7]
+        smk_RBP = sys.argv[8]
+
+        # NODES
+        sno = pd.read_csv(smk_snoRNA,sep='\t',usecols=['gene_id','gene_name','gene_biotype','min_TPM','max_TPM'])
+        rbp = pd.read_csv(smk_RBP,sep='\t',usecols=['gene_name','gene_biotype','min_TPM','max_TPM'])
+        rbp['gene_id'] = rbp['gene_name']
+        rbp = rbp[['gene_id','gene_name','gene_biotype','min_TPM','max_TPM']]
+
+        nodes = pd.concat([sno,rbp],ignore_index=True)
+        nodes.gene_name.fillna(nodes.gene_id, inplace=True)
+        nodes = node_weight(nodes)
+        nodes.to_csv(smk_nodes,sep='\t',index=None)
+
+        # EDGES
+        rbp_binds_to_sno = pd.read_csv(smk_RBP_binds_to_sno,sep='\t')
+        sno_rbp_ovlp = pd.read_csv(smk_sno_RBP_overlap,sep='\t')
+        string = pd.read_csv(smk_STRING,sep='\t')
+        edges = pd.concat([rbp_binds_to_sno,sno_rbp_ovlp,string],axis=0,ignore_index=True)
+        edges = edges[['source','target','interaction','ENCODE_log_pval','sno_RBP_overlap_log_pval','STRING_score']]
+        edges = edge_weight(edges)
+        edges.to_csv(smk_edges,sep='\t',index=None)
+    
+
+    elif smk_func == "build": # Build snoFlake using Cytoscape --> Cytoscape app has to be OPEN
+
+        smk_network = sys.argv[4]
+        nodes = pd.read_csv(smk_nodes,sep='\t')
+        nodes.rename(columns={"gene_id":"id","gene_name":"name","gene_biotype":"group","weight":"score"},inplace=True)
+        edges = pd.read_csv(smk_edges,sep='\t')
+
+        build_network(nodes,edges,smk_network)
 
 
 if __name__ == '__main__':
